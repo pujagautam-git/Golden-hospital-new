318e9775e845d0b4fff86e8f727806c8ddaf8d0a
pujagautam-git <pujagautam3945@gmail.com>
2025-09-19 17:25:59 +0545

    some changes on vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteCompression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteCompression()],
    build: {
     chunkSizeWarningLimit: 1000, // (optional) increase limit to avoid warnings
     rollupOptions: {
